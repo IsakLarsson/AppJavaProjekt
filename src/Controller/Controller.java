38aca9e419efb3ad25ec80b104b4fd1a4779a5d1
dcrjodle <joel.bystedt@gmail.com>
2019-12-02 14:18:32 +0100

    Skapat tester

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -12,34 +12,40 @@ import java.util.ArrayList;
 
 public class Controller {
 
-    private ArrayList<BufferedImage> frames;
+    // Map with positions
     private TileMap tileMap;
 
     public Controller(){
 
+        //
         SwingUtilities.invokeLater(() -> {
-            //ParseXML xmlParser = new ParseXML();
-            //tileMap = xmlParser.parser();
 
-            //frames = new ArrayList<>();
+            // A panel that you draw on
             GameWindow gameWindow = new GameWindow();
-            GameFrame gameFrame = new GameFrame("Test", gameWindow);
+
+            // A window frame containing a menubar
+            GameFrame gameFrame = new GameFrame("Game", gameWindow);
+
+            // Show the gui
             gameFrame.show();
 
+            // An adapter that acts a middlehand between the gui and the model
             ModelAdapter adapter = new ModelAdapter(gameWindow);
+
+            // A model
             Model model = new Model(adapter);
+
+            // Update the gui with 1 seconds intervals
             try {
                 Thread.sleep(1000);
             }
             catch (Exception e) {
                 e.printStackTrace();
             }
+
+            // Update the gui
             gameWindow.update();
 
-
         });
-
-
-
     }
 }
