624db9ddea2095a886138a91ee328926486204ba
Joel Bystedt <joel.bystedt@gmail.com>
2019-12-02 11:31:49 +0100

    Add files via upload

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -16,32 +16,12 @@ public class Controller {
     public Controller(){
 
         SwingUtilities.invokeLater(() -> {
-            GameFrame gui = new GameFrame("Anti-TD");
-            gui.show();
+            frames = new ArrayList<>();
+            Boolean gameIsRunning = true;
+            GameWindow gameWindow = new GameWindow();
+            GameFrame gameFrame = new GameFrame("Test", gameWindow);
+            gameFrame.show();
         });
-
-        /* Skapa en tråd för att uppdatera GUI
-        Thread thread = new Thread(() -> {
-            //15 sekunder
-            for (int i=0; i < 150; i++) {
-                ArrayList<Integer> a = new ArrayList<>();
-                a.add(i);
-                a.add(i);
-                a.add(10);
-                a.add(10);
-
-                gameWindow.setPositions(a);    //Uppdatera positions
-                gameWindow.update();                //Uppdatera window
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-        */
 
 
 
