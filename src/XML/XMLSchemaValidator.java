09a761d99de04498b10117285c00cc7a6e0b4b3e
Albin Jönsson <aljo0098@gapps.umu.se>
2019-12-05 10:31:27 +0100

    Minor changes in XMLSchema.xsd

--- src/XML/XMLSchemaValidator.java
+++ src/XML/XMLSchemaValidator.java
@@ -7,7 +7,6 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
 import java.io.IOException;
 
 public class XMLSchemaValidator {
