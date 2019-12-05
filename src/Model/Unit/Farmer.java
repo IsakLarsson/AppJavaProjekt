d494fc85ee06955a2433d5ffc62ed27f2fe1438d
Axel Jakobsson <c18ajn@cs.umu.se>
2019-12-05 15:45:35 +0100

    Added images for farmer and "soldier"

--- src/Model/Unit/Farmer.java
+++ src/Model/Unit/Farmer.java
@@ -1,10 +1,14 @@
 package Model.Unit;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 import static java.awt.Color.white;
 
 public class Farmer implements Unit {
+
+    private Image image;
     private int hp = 50;
     private int speed = 20;
     private int dmg = 1;
@@ -14,6 +18,13 @@ public class Farmer implements Unit {
     private int x,y;
 
     public Farmer() {
+        try {
+            image = ImageIO.read(
+                    this.getClass().getResourceAsStream("/GUI/images/Farmer.png"));
+            image = image.getScaledInstance(50,50,Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -38,7 +49,14 @@ public class Farmer implements Unit {
         return dmg;
     }
 
+    @Override
+    public Image getImage() {
+        return image;
+    }
+
     public int getSpeed(){
         return speed;
     }
+
+
 }
